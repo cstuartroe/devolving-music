49277d61a730a95a25b62215ae2993bb0079c181
Conor Stuart Roe <cstuartroe@haverford.edu>
2022-06-06 20:42:21 -0400

    Bump django version

--- devolving_music/views/song_submissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from devolving_music.models.song_submission import SongSubmission
-from devolving_music.models.serializers.song_submission import SongSubmissionSerializer
-
-
-class SongSubmissionViewSet(viewsets.ModelViewSet):
-    queryset = SongSubmission.objects.all()
-    serializer_class = SongSubmissionSerializer

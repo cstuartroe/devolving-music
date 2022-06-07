f6b553d945d5ba2eacc6eb13227fa2755f84a96c
Conor Stuart Roe <cstuartroe@haverford.edu>
2022-06-06 20:09:36 -0400

    Permit music.devolving.party domain

--- devolving_music/settings.py
+++ devolving_music/settings.py
@@ -1,5 +1,4 @@
 import os
-from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -7,6 +6,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'devolving-music.herokuapp.com',
+    'music.devolving.party',
 ]
 
 DEBUG = bool(os.getenv('DEBUG'))

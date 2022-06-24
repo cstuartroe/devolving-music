c6aeec9c617bb9896f7aba5a5d467dca1d511dcb
sfedrick <sfedrick@haverford.edu>
2022-06-24 01:04:04 -0400

    conor's gut sucks

--- devolving_music/views/get_song_pair.py
+++ devolving_music/views/get_song_pair.py
@@ -4,14 +4,13 @@ from devolving_music.lib.song_utils import get_song_color
 from devolving_music.models.event import Event
 from devolving_music.models.song_submission import SongSubmission
 from devolving_music.models.serializers.song_submission import SongSubmissionSerializer
-from devolving_music.lib.song_scores import ScoreSuite
 
 
 class GetSongPairView(View):
     @safe_url_params
     def get(self, _request, event: Event):
 
-        voteable_submissions = ScoreSuite.get_voteable_submissions(event)
+        voteable_submissions = SongSubmission.get_voteable_submissions(event)
 
         if len(voteable_submissions) < 2:
             return failure("Not enough songs have been submitted for this event.")

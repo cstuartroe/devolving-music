1aafa4eb0ee1c6bb325d290d98b6c6a79c871391
sfedrick <sfedrick@haverford.edu>
2022-06-28 09:10:07 -0400

    completed get song pair mvp

--- devolving_music/lib/song_scores.py
+++ devolving_music/lib/song_scores.py
@@ -1,6 +1,6 @@
 from cmath import inf
 from typing import Iterable
-from bisect import bisect_left
+import random
 
 
 from devolving_music.lib.elo_scoring import elo_rating
@@ -18,46 +18,8 @@ class SongScores():
 
         self.comparison_submissions = ScoreSuite.get_event_comparisons(event)
 
-    @staticmethod
-    def compare_not_found(
-            comparison_submission: "SongComparison",
-            song1: "ScoreObject",
-            song2: "ScoreObject"):
-
-        return not song1.compare_present(
-            comparison_submission) and not song2.compare_present(comparison_submission)
-
-    @staticmethod
-    def update_song_rating(
-            comparison_submission: "SongComparison",
-            song1: "ScoreObject",
-            song2: "ScoreObject",
-            score_range=30):
-
-        if SongScores.compare_not_found(comparison_submission, song1, song2):
-
-            song1.log_comparison(comparison_submission)
-
-            song2.log_comparison(comparison_submission)
-
-            song1.quality_score, song2.quality_score = elo_rating(
-                song1.quality_score, song2.quality_score, score_range, comparison_submission.first_better)
-
-            song1.energy_score, song2.energy_score = elo_rating(
-                song1.energy_score, song2.energy_score, score_range, comparison_submission.first_peakier)
-
-            song1.post_peak_score, song2.post_peak_score = elo_rating(
-                song1.post_peak_score, song2.post_peak_score, score_range, comparison_submission.first_post_peakier)
-
-
     def get_scores(self):
-
-        # song_submissions song_submissions should be a list of
-
-        # song submissions ordered from least to greatest
-
-        # by songsubmission id
-
+        #calculates scores for all submissions using current comparisons
         for compare in self.comparison_submissions:
 
             song1_index = compare.first_submission.id
@@ -72,29 +34,13 @@ class SongScores():
 
         return self.song_score_dict
     
-    @staticmethod
-    #sorts in ascending order
-    def get_info_sort(song_dict: "dict[int, ScoreSuite]"):
-        info_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).info_score)
-        # return list of keys of dictionary of song objects sorted by info
-        return info_submissions
+    def get_compare_submission(self, submission_key):
+        key_list = list(self.song_score_dict.keys())
+        key_list.remove(submission_key)
+        # once you have a critical number of comparisons then pull from quality list
+        return random.choice(key_list)
 
-    @staticmethod
-    #sorts in ascending order
-    def get_energy_sort(song_dict: "dict[int, ScoreSuite]"):
-        energy_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).energy_score if song_dict.get(sub).energy_score is not None else -inf)
-        # return list of keys of dictionary of song objects sorted by energy
-        return energy_submissions
-        
-    @staticmethod
-    #sorts in ascending order
-    def get_peak_sort(song_dict: "dict[int, ScoreSuite]"):
-        peak_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).post_peak_score  if song_dict.get(sub).post_peak_score is not None else -inf)
-        # return list of keys of dictionary of song objects sorted by peakyness
-        return peak_submissions
-
-    @staticmethod
-    def mvg_avg(song_submissions_sorted: Iterable["SongSubmission"]):
+    def mvg_avg(self,song_submissions_sorted: Iterable["SongSubmission"]):
 
         mvg_avg = [None] * len(song_submissions_sorted)
 
@@ -107,8 +53,7 @@ class SongScores():
 
         return mvg_avg
 
-    @staticmethod
-    def check_quality(
+    def check_quality(self,
             song_submissions_sorted: Iterable["SongSubmission"],
             remove: int):
 
@@ -164,3 +109,58 @@ class SongScores():
         # final_quality_list=remove(energy_sorted,remove_index)
 
         return final_quality_list
+
+    ###
+    # static methods can be moved into helper function
+    ###
+    @staticmethod
+    #sorts in ascending order
+    def get_info_sort(song_dict: "dict[int, ScoreSuite]"):
+        info_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).info_score)
+        # return list of keys of dictionary of song objects sorted by info
+        return info_submissions
+
+    @staticmethod
+    #sorts in ascending order
+    def get_energy_sort(song_dict: "dict[int, ScoreSuite]"):
+        energy_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).energy_score if song_dict.get(sub).energy_score is not None else -inf)
+        # return list of keys of dictionary of song objects sorted by energy
+        return energy_submissions
+        
+    @staticmethod
+    #sorts in ascending order
+    def get_peak_sort(song_dict: "dict[int, ScoreSuite]"):
+        peak_submissions = sorted(song_dict,key=lambda sub: song_dict.get(sub).post_peak_score  if song_dict.get(sub).post_peak_score is not None else -inf)
+        # return list of keys of dictionary of song objects sorted by peakyness
+        return peak_submissions
+
+    @staticmethod
+    def compare_not_found(
+            comparison_submission: "SongComparison",
+            song1: "ScoreObject",
+            song2: "ScoreObject"):
+
+        return not song1.compare_present(comparison_submission) and not song2.compare_present(comparison_submission)
+
+    @staticmethod
+    def update_song_rating(
+            comparison_submission: "SongComparison",
+            song1: "ScoreObject",
+            song2: "ScoreObject",
+            score_range=30):
+
+        if SongScores.compare_not_found(comparison_submission, song1, song2):
+
+            song1.log_comparison(comparison_submission)
+
+            song2.log_comparison(comparison_submission)
+
+            song1.quality_score, song2.quality_score = elo_rating(
+                song1.quality_score, song2.quality_score, score_range, comparison_submission.first_better)
+
+            song1.energy_score, song2.energy_score = elo_rating(
+                song1.energy_score, song2.energy_score, score_range, comparison_submission.first_peakier)
+
+            song1.post_peak_score, song2.post_peak_score = elo_rating(
+                song1.post_peak_score, song2.post_peak_score, score_range, comparison_submission.first_post_peakier)
+
--- devolving_music/views/get_song_pair.py
+++ devolving_music/views/get_song_pair.py
@@ -1,10 +1,12 @@
 from django.views import View
 
 from devolving_music.lib.score_object import ScoreSuite
+from devolving_music.lib.song_scores import SongScores
 from .param_utils import safe_url_params, success, failure
 from devolving_music.lib.song_utils import get_song_color
 from devolving_music.models.event import Event
 from devolving_music.models.serializers.song_submission import SongSubmissionSerializer
+from devolving_music.models.song_submission import SongSubmission
 
 
 class GetSongPairView(View):
@@ -16,7 +18,16 @@ class GetSongPairView(View):
         if len(voteable_submissions) < 2:
             return failure("Not enough songs have been submitted for this event.")
 
-        sub1, sub2 = voteable_submissions[:2]
+        Scores = SongScores(event)
+        Scores_list = Scores.get_scores()
+        info_list = SongScores.get_info_sort(Scores_list)
+        sub2_id = Scores.get_compare_submission(info_list[0])
+
+
+        #grab submission with least ammount of information
+        sub1 = SongSubmission.objects.get(id = info_list[0])
+        #grab random submission
+        sub2 = SongSubmission.objects.get(id = sub2_id)
 
         return success({
             "sub1": SongSubmissionSerializer(sub1).data,

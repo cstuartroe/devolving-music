7f565eeb7e5bdd8da11452752f93d9e57a234324
sfedrick <shaunclfedrick@gmail.com>
2022-08-24 13:35:21 -0400

    Tweak header formatting (#23) (#25)
    Co-authored-by: Conor Stuart Roe <cstuartroe@haverford.edu>

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component, CSSProperties} from "react";
 import { Link } from "react-router-dom";
 import { Event } from "./models";
 import ResourceManager from "./ResourceManager";
@@ -26,12 +26,14 @@ export default class Header extends Component<HeaderProps, HeaderState>{
     ResourceManager.getModels("events").then(events => this.setState({events}))
   }
 
-  renderMenuItem(to: string, text: string) {
+  renderMenuItem(to: string, text: string, style?: CSSProperties) {
     return (
-      <div className={`menu-item`}>
-        <Link to={to} className="center">
-          {text}
-        </Link>
+      <div className="menu-item" style={style}>
+        <div>
+          <Link to={to}>
+            {text}
+          </Link>
+        </div>
       </div>
     );
   }
@@ -46,13 +48,7 @@ export default class Header extends Component<HeaderProps, HeaderState>{
           </div>
         </div>
         <div className="row header">
-          <div className={`menu-item`}>
-            <div className="header-logo">
-              <Link to="/">
-                {this.props.event.name.toLowerCase().replace(':', '\n')}
-              </Link>
-            </div>
-          </div>
+          {this.renderMenuItem("/", this.props.event.name, {fontWeight: 800})}
           {this.state.events.length > 1 && this.renderMenuItem("/event-selector", "Switch events")}
           {this.renderMenuItem("/submit-playlist", "Submit songs")}
           {this.renderMenuItem("/rate", "Rate songs")}

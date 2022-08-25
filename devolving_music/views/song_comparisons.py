637f2fe888a523063090a0f76d50399dd4626d6c
Conor Stuart Roe <cstuartroe@haverford.edu>
2022-08-25 00:54:47 -0400

    wip

--- devolving_music/views/song_comparisons.py
+++ devolving_music/views/song_comparisons.py
@@ -13,8 +13,9 @@ from .param_utils import success, failure
 
 
 VOTE_QUOTA_PER_SUB = 10
-MAX_VOTE_RATIO = 1.25
-EXTRA_VOTES = 200
+NUM_CORE_CONTRIBUTORS = 4
+MAX_VOTE_RATIO = 2
+EXTRA_VOTES_PER_SUB = .5
 
 
 class SongComparisonsView(LoginRequiredMixin, APIView):
@@ -25,10 +26,9 @@ class SongComparisonsView(LoginRequiredMixin, APIView):
 
     def post(self, request):
         event_id = SongSubmission.objects.get(id=request.data['first_submission_id']).event_id
+        num_submissions = SongSubmission.objects.filter(event_id=event_id).count()
 
         # Limit the number of votes based on the number of song submissions
-
-        num_submissions = SongSubmission.objects.filter(event_id=event_id).count()
 
         votes = SongComparison.objects.filter(
             voter=request.user, created_at__gte=(timezone.now() - timedelta(hours=24))).count()
@@ -42,9 +42,9 @@ class SongComparisonsView(LoginRequiredMixin, APIView):
             SongComparison.objects.select_related('first_submission').filter(first_submission__event_id=event_id)
             .values('voter_id').annotate(vcount=Count('id')).order_by('-vcount'))
 
-        print(votes_by_user)
-
-        max_relative_votes = (votes_by_user[1]['vcount']*MAX_VOTE_RATIO + EXTRA_VOTES)
+        top_vote_numbers = [v['vcount'] for v in votes_by_user[:NUM_CORE_CONTRIBUTORS]]
+        average_votes = sum(top_vote_numbers)/len(top_vote_numbers)
+        max_relative_votes = (average_votes*MAX_VOTE_RATIO) + (num_submissions*EXTRA_VOTES_PER_SUB)
 
         if (votes_by_user[0]['voter_id'] == request.user.id) and (votes_by_user[0]['vcount'] >= max_relative_votes):
             return failure("You have voted too much relative to others. Please give them a chance to catch up.")
